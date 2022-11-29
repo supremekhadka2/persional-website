8f582c1853677487e4e74e1b30a43b3b74b59ba1
Supreme <supremekhadka2@gmail.com>
2022-11-29 23:05:46 +0545

    internal pages customized

--- js/script.js
+++ js/script.js
@@ -79,3 +79,12 @@ menuCloseBtn.addEventListener("click", closeMenu);
 menuLink.forEach((e) => {
   e.addEventListener("click", closeMenu);
 });
+
+//PRELOADER
+const preloader = document.getElementById("preloader");
+
+document.addEventListener("DOMContentLoaded", function () {
+  preloader.style.opacity = "0";
+  preloader.style.zIndex = "-100";
+  preloader.style.transition = ".5s ease-out";
+});
